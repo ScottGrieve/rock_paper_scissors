2179ae1ac77267f129a662dd95ec846b2bf0c33d
ScottGrieve <pokecarft@gmail.com>
2021-06-27 10:51:41 +0100

    Added the playRound function

--- script.js
+++ script.js
@@ -11,6 +11,38 @@ function computerPlay() {
     }
 
 }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+        return 'Tie! You both chose rock.';
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You lose! Paper beats rock.';
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You win! Rock beats paper. ';
+    }
+    else if (playerSelection == 'paper' && computerPlay() == 'paper') {
+        return 'Tie! You both chose paper.';
+    }
+    else if (playerSelection == 'paper' && computerPlay() == 'rock') {
+        return 'You win! Paper beats rock.';
+    }
+    else if (playerSelection == 'paper' && computerPlay() == 'scissors') {
+        return 'You lose! Scissors beats paper.';
+    }
+    else if (playerSelection == 'scissors' && computerPlay() == 'scissors') {
+        return 'Tie! You both chose scissors.';
+    }
+    else if (playerSelection == 'scissors' && computerPlay() == 'rock') {
+        return 'You lose! Rock beats scissors.';
+    }
+    else if (playerSelection == 'scissors' && computerPlay() == 'paper') {
+        return 'You win! Scissors beats paper';
+    }
+}
 
-console.log(computerPlay());
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
 
