9d30d48ff0b4762ea7a2195e8466b8888583d72a
ScottGrieve <pokecarft@gmail.com>
2021-11-18 21:13:20 +0000

    Rewrote the JS, CSS still needs reworking

--- script.js
+++ script.js
@@ -1,82 +1,56 @@
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
+
 function computerPlay() {
-    //generate a random number from 1-100
-    let randomNum = Math.floor(Math.random() * 100 + 1);
-    // create control flow that sets computer choice based on number from 1-33, 34-66, 66-100
-    // with 1-33 rock, 34-66 paper, 67 -100 scissor
-    // return rock, paper, or scissor
-    if (randomNum <= 33) {
-      return "rock";
-    } else if (randomNum >= 34 && randomNum <= 66) {
-      return "paper";
-    } else {
-      return "scissor";
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
-  }
-  
-  function playerPlay() {
-  
-    let response = prompt("please type rock, paper, or scissor!");
-    return response.toLowerCase();
-  }
-  
-  function singleRound(playerSelection, computerSelection) {
-  
-    let result = checkWinner(playerSelection, computerSelection);
-  
-    if (result === "tie") {
-      return `Tie! ${playerSelection} ties ${computerSelection}.`;
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
-  
-    let msg = result
-      ? `You win! ${playerSelection} beats ${computerSelection}.`
-      : `You lose! ${computerSelection} beats ${playerSelection}.`;
-  
-    return msg;
-  }
-  
-  function checkWinner(playerpick, computerpick) {
-    if (playerpick === computerpick) {
-      return "tie";
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
-  
-    if (playerpick === "rock" && computerpick === "scissor") {
-      return true;
-    }
-  
-    if (playerpick === "rock" && computerpick === "paper") {
-      return false;
-    }
-  
-    if (playerpick === "paper" && computerpick === "scissor") {
-      return false;
-    }
-  
-    if (playerpick === "paper" && computerpick === "rock") {
-      return true;
-    }
-  
-    if (playerpick === "scissor" && computerpick === "rock") {
-      return false;
-    }
-  
-    if (playerpick === "scissor" && computerpick === "paper") {
-      return true;
-    }
-  }
-  
-  function game() {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay();
-    console.log(singleRound(playerSelection, computerSelection));
-  }
-  
-  const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-  
-  async function runGame() {
-    for (var i = 0; i < 5; i++) {
-      game();
-      await timer(1500); 
-    }
-  }
-  
-  runGame();
+
+    document.getElementById('result').innerHTML = result
+    return
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
